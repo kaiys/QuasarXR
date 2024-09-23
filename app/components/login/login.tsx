c371938c9d1360fd97edbfabcdaae4b631aea146
YoungHoon <kai87yh@gmail.com>
2024-09-24 02:03:23 +0900

    modify login form

--- app/components/login/login.tsx
+++ app/components/login/login.tsx
@@ -16,23 +16,41 @@ interface Props {
 
 const normalBorder = '1px solid #319cd0';
 const errorBorder = '1px solid #ff3131';
+const normalBg = '#efefef';
+const errorBg = 'rgba(255, 150, 150, 0.25)';
 
 const LoginForm = ({ isLogin, setIsLogin } : Props) => {
-    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [ passwordVisible, setPasswordVisible ] = useState(false);
     const [ emailBorder, setEmailBorder ] = useState(normalBorder);    
     const [ passwordBorder, setPasswordBorder ] = useState(normalBorder);
+    const [ emailBg, setEmailBg ] = useState(normalBg);
+    const [ pwBg, setpwBg ] = useState(normalBg);
     const initialValue : LoginFormState = { errors: {} }
-    const [state, formAction, isPending ] = useFormState(login, initialValue);
+    const [ state, formAction, isPending ] = useFormState(login, initialValue);
     const router = useRouter();
 
     useEffect( () => {
-        console.log( state.redirect );
         if ( state.redirect ) {
             // login successful
             setIsLogin(false);
             router.push( state.redirect );
         }
-            
+
+        if ( state.errors?.email ) {
+            setEmailBorder( errorBorder );
+            setEmailBg( errorBg );
+        } else {
+            setEmailBorder( normalBorder );
+            setEmailBg( normalBg );
+        }
+
+        if ( state.errors?.password ) {
+            setPasswordBorder( errorBorder );
+            setpwBg( errorBg );
+        } else {
+            setPasswordBorder( normalBorder );
+            setpwBg( normalBg );
+        }
     }, [ state ] );
 
     const loginRef = React.useRef<HTMLFormElement>(null);
@@ -54,13 +72,13 @@ const LoginForm = ({ isLogin, setIsLogin } : Props) => {
                 <h2>LOGIN</h2>
                 <div>
                     <label>Email</label>
-                    <input className={styles.authInput} style={{ border: emailBorder }} name="email" placeholder="user@Email.com" autoComplete="username"/>
+                    <input className={styles.authInput} style={{ border: emailBorder, background: emailBg }} name="email" placeholder="user@Email.com" autoComplete="username"/>
                 </div>
                 <div>
                     <label>Password</label>
                     <section>
                         <div style={{ position: 'relative' }}>
-                            <input className={styles.authInput} style={{ border: passwordBorder }}
+                            <input className={styles.authInput} style={{ border: passwordBorder, background: pwBg }}
                                 type={passwordVisible ? 'text' : 'password'}
                                 placeholder="enter your password"
                                 autoComplete="current-password"
@@ -80,13 +98,9 @@ const LoginForm = ({ isLogin, setIsLogin } : Props) => {
                         </div>
                     </section>
                 </div>
-                <div style={{margin: '0px 0px 0px 5px', height: '30px', color: "red", fontSize: '12px'}}>
-                    { state?.errors?.email && <p style={{margin: '0'}}>{"email information is not valid"}</p> }
-                    { state?.errors?.password && <p style={{margin: '0'}}>{"incorrect password, put right password again"}</p> }
-                    {/* {errors?.username?.type === 'required' && <p style={{margin: '0'}}>{"This field is required"}</p>}
-                    {errors?.username?.type === 'database' && <p style={{margin: '0'}}>{"This username is not available"}</p>}
-                    {errors?.username?.type === 'minLength' && <p style={{margin: '0'}}>{"username too short"}</p>}
-                    {errors?.username?.type === 'maxLength' && <p style={{margin: '0'}}>{"username too long"}</p>} */}
+                <div style={{margin: '0px 0px 0px 5px', height: 'auto', color: "red", fontSize: '12px'}}>
+                    { state?.errors?.email && <p style={{margin: '5px'}}>{"email information is not valid"}</p> }
+                    { state?.errors?.password && <p style={{margin: '5px'}}>{"incorrect password, put right password again"}</p> }
                 </div>
                 <button type="submit" disabled={isPending}>LOGIN</button>
             </section>
